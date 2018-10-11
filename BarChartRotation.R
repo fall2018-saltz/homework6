ce9369edc865195deda2c00ca73376843d44a3ef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:31:46 -0400

    Saving BarChartRotation.R

--- BarChartRotation.R
+++ BarChartRotation.R
@@ -1,2 +1,6 @@
 
 library(ggplot2)
+gbarchart <- ggplot(mergedataframe,aes(x=reorder(stateName,newcolumn), y=newcolumn, group=1))
+gbarchart <- gbarchart + geom_col()
+gbarchart <- gbarchart + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+gbarchart
