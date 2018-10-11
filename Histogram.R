3025c6a01143c1351ab98d64f41fb40722332d76
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:56:32 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -1,4 +1,5 @@
 
+library(ggplot2)
 g <- ggplot(mergedataframe, aes(x=population)) 
 g <- g +  geom_histogram(binwidth = 50000000, color="black",fill="white")
 g <- g + ggtitle("Histogram by Population")
