cc6102668225ea43649e7bb5a57a1996e83450da
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:06:04 -0400

    Saving Histogram3.R

--- Histogram3.R
+++ Histogram3.R
@@ -1,1 +1,6 @@
 
+library(ggplot)
+gassault <- ggplot(mergedataframe, aes(x=population)) 
+gassault <- gassault +  geom_histogram(binwidth = 1, color="black",fill="white")
+gassault <- gassault + ggtitle("Histogram by Assault")
+gassault
