331ea06bc9c9e1488a2cb25243085d2b6063e9f9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:18:53 -0400

    Saving Histogram2.R

--- Histogram2.R
+++ Histogram2.R
@@ -1,6 +1,6 @@
 
 library(ggplot2)
-gmurder <- ggplot(mergedataframe, aes(x=Mudrer)) 
+gmurder <- ggplot(mergedataframe, aes(x=Murder)) 
 gmudder <- gmurder +  geom_histogram(binwidth = 1, color="black",fill="white")
 gmurder <- gmurder + ggtitle("Histogram by Murder")
 gmurder
