af10dc781869eba9d50368eee2607a56a5bdad31
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:11:32 -0400

    Saving Boxplot2.R

--- Boxplot2.R
+++ Boxplot2.R
@@ -2,3 +2,6 @@
 library(ggplot)
 gboxmur <- ggplot(mergedataframe, aes(x=factor(0),Murder)) + geom_boxplot()
 gboxmur
+
+
+#The visualization of data can be done easier in Histogram as the histogram properly shows all values.
