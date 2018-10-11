c0d05e547a1995393b483d2603af2bcd3dd77ecb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:00:47 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -4,3 +4,7 @@ g <- ggplot(mergedataframe, aes(x=population))
 g <- g +  geom_histogram(binwidth = 1, color="black",fill="white")
 g <- g + ggtitle("Histogram by Population")
 g
+
+gmurder <- ggplot(mergedataframe, aes(x=Mudrer)) 
+gmudrder <- g +  geom_histogram(binwidth = 1, color="black",fill="white")
+gmurder <- g + ggtitle("Histogram by Murder")
