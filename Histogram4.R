27e7b3e953df4ffb879cd7abc54ee4d2d00cba63
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:27:08 -0400

    Saving Histogram4.R

--- Histogram4.R
+++ Histogram4.R
@@ -1,6 +1,6 @@
 
 library(ggplot2)
-grape <- ggplot(mergedataframe, aes(x=population)) 
+grape <- ggplot(mergedataframe, aes(x=Rape)) 
 grape <- grape +  geom_histogram(binwidth = 1, color="black",fill="white")
 grape <- grape + ggtitle("Histogram by Rape")
 grape
