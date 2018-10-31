9b18abb3ff1b9c207ea737bb6439f598f4a72146
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 15:18:34 -0400

    Saving BarchartStateMurder.R

--- BarchartStateMurder.R
+++ BarchartStateMurder.R
@@ -3,4 +3,4 @@ library(ggplot2)
 gbarchart1 <- ggplot(mergedataframe,aes(x=reorder(stateName,Murder), y=newcolumn, group=1))
 gbarchart1 <- gbarchart1 + geom_col()
 gbarchart1 <- gbarchart1 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-gbarchart1
+#gbarchart1
